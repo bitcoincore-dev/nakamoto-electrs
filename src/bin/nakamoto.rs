53b2ecee1eeaca9b859a5a80ca08bc8aab513419
randymcmillan <randymcmillan@protonmail.com>
2026-08-20 11:35:35 -0400

    src/bin

--- src/bin/nakamoto.rs
+++
@@ -1,6 +0,0 @@
-use anyhow::Result;
-use nakamoto_electrs::{Network, app::run_nakamoto, config::NakamotoConfig};
-
-fn main() -> Result<()> {
-    run_nakamoto(NakamotoConfig::new(Network::Testnet))
-}
